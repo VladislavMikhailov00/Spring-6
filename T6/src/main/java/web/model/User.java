891e5aa6b23167570340d380070e240f1645469b
Mikhailov Vladislav <binoklev00@mail.ru>
2025-05-14 20:31:06 +0300

    Add files via upload

---
+++ T6/src/main/java/web/model/User.java
@@ -0,0 +1,41 @@
+package web.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "users")
+public class User {
+
+    private @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+
+    @Column(name = "name")
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 35, message = "Name should be between 2 to 35")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Email(message = "Email should be valid")
+    private String email;
+
+    public User() {}
+
+    public User(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+}
---
+++ T6/src/main/java/web/service/UserServiceImp.java
@@ -0,0 +1,49 @@
+package web.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import web.dao.UserDao;
+import web.model.User;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class UserServiceImp implements UserService {
+
+    private final UserDao userDao;
+
+    public UserServiceImp(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userDao.getAllUsers();
+    }
+
+    @Override
+    public void createOrUpdateUser(User user) {
+        if (0 == user.getId()) {
+            userDao.createUser(user);
+        } else {
+            userDao.updateUser(user);
+        }
+    }
+
+    @Override
+    public User readUser(long id) {
+        return userDao.readUser(id);
+    }
+
+    @Override
+    public User deleteUser(long id) {
+        User user = null;
+        try {
+            user = userDao.deleteUser(id);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
+}
